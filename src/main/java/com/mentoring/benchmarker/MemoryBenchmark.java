b661cf324d8c2be68497b4adcb37f91e295826ed
taras <tgrynchuk@gmail.com>
2016-03-15 16:14:59 +0200

    last collections

--- src/main/java/com/mentoring/benchmarker/MemoryBenchmark.java
+++ src/main/java/com/mentoring/benchmarker/MemoryBenchmark.java
@@ -7,13 +7,18 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentSkipListMap;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLongArray;
 
+import com.google.common.base.Stopwatch;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
 import com.google.common.collect.BiMap;
+import com.google.common.collect.ConcurrentHashMultiset;
 import com.google.common.collect.HashBasedTable;
 import com.google.common.collect.HashBiMap;
 import com.google.common.collect.HashMultimap;
@@ -24,22 +29,53 @@ import com.google.common.collect.Table;
 import com.google.common.util.concurrent.AtomicLongMap;
 
 public class MemoryBenchmark {
-	public static long SIZE = 10000;
+	public static long SIZE = 100000;
 	
 	public static void main(String[] args) throws Exception {
 		System.out.println("List memory benchmarking:");
 		System.out.println("Capacity: " + SIZE);
 		
+
+//		
+		
+//		long foo = 0;
+//		for(int i=0; i<100000; i++) {
+//			foo +=i;
+//		}
+//		
+//		System.out.println(foo);
+		
+		
+		List<Integer> arrayList = new ArrayList<Integer>();
+		listSizeBenchmark(arrayList);
+		
 //		List<Integer> linkedList = new LinkedList<Integer>();
 //		listSizeBenchmark(linkedList);
-//		
-//		
-//		List<Integer> arrayList = new ArrayList<Integer>();
-//		listSizeBenchmark(arrayList);
 		
-		Map<Integer, Integer> hashMap =  new HashMap<>();
-		hashMapSizeBenchmark(hashMap);
 
+		
+
+
+		
+		
+//		
+//		
+
+		
+
+//		Map<Integer, Integer> synchMap = Collections.synchronizedMap(new HashMap<>());
+//		hashMapSizeBenchmark(synchMap);
+		
+		
+//		Map<Integer, Integer> hashMap =  new HashMap<>();
+//		hashMapSizeBenchmark(hashMap);
+
+//		Map<Integer, Integer> concurrentHashMap =  new  ConcurrentHashMap<>();
+//		hashMapSizeBenchmark(concurrentHashMap);
+		
+
+//		ConcurrentSkipListMap<Integer, Integer>  concurrentSkipListMap  =  new ConcurrentSkipListMap<>();
+//		concurrentSkipListMapSizeBenchmark(concurrentSkipListMap);
 
 //		Multiset<Integer> multiset = HashMultiset.create();
 //		multiSetSizeBenchmark(multiset);
@@ -52,7 +88,13 @@ public class MemoryBenchmark {
 //		
 //		Multimap<Integer, Integer> multimap = HashMultimap.create();
 //		multiMapSizeBenchmark(multimap);
-//		
+		
+		
+//		ConcurrentHashMultiset<Integer> concurrentHashMultiset = ConcurrentHashMultiset.create();
+//		concurrentHashMultisetSizeBenchmark(concurrentHashMultiset);
+		
+		
+		
 //		BiMap<Integer, Integer> biMap = HashBiMap.create();
 //		biMapSizeBenchmark(biMap);
 		
@@ -60,27 +102,32 @@ public class MemoryBenchmark {
 //		tableSizeBenchmark(table);
 		
 		
-		LoadingCache<Integer, Integer> loadingCache = 
-		         CacheBuilder.newBuilder()
-		            .maximumSize(10) // maximum 100 records can be cached
-		            .expireAfterAccess(30, TimeUnit.MINUTES) // cache will expire after 30 minutes of access
-		            .build(new CacheLoader<Integer, Integer>() { // build the cacheloader
-		            
-		               @Override
-		               public Integer load(Integer id) throws Exception {
-		                  //make the expensive call
-		                  return id;
-		            	   
-		               } 
-		            });
 		
-		loadingCacheSizeBenchmark(loadingCache);
+
 		
-		System.out.println("loadingCache = ");
-		for(int i=0; i<loadingCache.size(); i++) {
-			System.out.println(loadingCache.getUnchecked(i));
-			
-		}
+//		LoadingCache<Integer, Integer> loadingCache = 
+//		         CacheBuilder.newBuilder()
+//		            .maximumSize(10) // maximum 100 records can be cached
+//		            .expireAfterAccess(30, TimeUnit.MINUTES) // cache will expire after 30 minutes of access
+//		            .build(new CacheLoader<Integer, Integer>() { // build the cacheloader
+//		            
+//		               @Override
+//		               public Integer load(Integer id) throws Exception {
+//		                  //make the expensive call
+//		                  return id;
+//		            	   
+//		               } 
+//		            });
+//		
+//		loadingCacheSizeBenchmark(loadingCache);
+//		
+//		System.out.println("loadingCache = ");
+//		for(int i=0; i<loadingCache.size(); i++) {
+//			System.out.println(loadingCache.getUnchecked(i));
+//			
+//		}
+		
+		
 		
 	}
 	
@@ -91,19 +138,31 @@ public class MemoryBenchmark {
 		System.err.println("gc works...");
 		super.finalize();
 	}
+
+
 	
 	public static void listSizeBenchmark(List<Integer> list) {
 		new MemoryBenchmark();
 		System.out.println("\nlistSizeBenchmark() -> " + list.getClass().getSimpleName());
-		long free = Runtime.getRuntime().freeMemory();
+		
+	
+		
+		long free =   Runtime.getRuntime().freeMemory();
 		long freeAfter;
 		
+		Stopwatch stopwatch = Stopwatch.createStarted();
 		for(int i = 0; i < SIZE; i++) {
 			list.add(i);
 		}
+		stopwatch.stop();
 		
 		freeAfter = Runtime.getRuntime().freeMemory();
+		
+		long nanos = stopwatch.elapsed(TimeUnit.NANOSECONDS);
+		
+		
 		System.out.println("Used mem: " + (free - freeAfter));
+		System.out.println("Nanos: " + nanos + " /" + stopwatch + "/");
 		
 	}
 	
@@ -134,11 +193,23 @@ public class MemoryBenchmark {
 		
 		freeAfter = Runtime.getRuntime().freeMemory();
 		System.out.println("Used mem: " + (free - freeAfter));
-		
 	}
 	
+	public static void concurrentHashMultisetSizeBenchmark(ConcurrentHashMultiset<Integer> concurrentHashMultiset) {
+		new MemoryBenchmark();
+		System.out.println("\nlistSizeBenchmark() -> " + concurrentHashMultiset.getClass().getSimpleName());
+		long free = Runtime.getRuntime().freeMemory();
+		long freeAfter;
+		
+		for(int i = 0; i < SIZE; i++) {
+			concurrentHashMultiset.add(i);
+		}
+		
+		freeAfter = Runtime.getRuntime().freeMemory();
+		System.out.println("Used mem: " + (free - freeAfter));
+	}	
 
-	private static void atomicMapBenchmark(AtomicLongMap<Integer> atomicMap) {
+	public static void atomicMapBenchmark(AtomicLongMap<Integer> atomicMap) {
 		new MemoryBenchmark();
 		System.out.println("\nlistSizeBenchmark() -> " + atomicMap.getClass().getSimpleName());
 		long free = Runtime.getRuntime().freeMemory();
@@ -153,7 +224,7 @@ public class MemoryBenchmark {
 		
 	}
 	
-	private static void multiMapSizeBenchmark(Multimap<Integer, Integer> multimap) {
+	public static void multiMapSizeBenchmark(Multimap<Integer, Integer> multimap) {
 		new MemoryBenchmark();
 		System.out.println("\nlistSizeBenchmark() -> " + multimap.getClass().getSimpleName());
 		long free = Runtime.getRuntime().freeMemory();
@@ -169,7 +240,7 @@ public class MemoryBenchmark {
 	}
 	
 
-	private static void biMapSizeBenchmark(BiMap<Integer, Integer> biMap) {
+	public static void biMapSizeBenchmark(BiMap<Integer, Integer> biMap) {
 		new MemoryBenchmark();
 		System.out.println("\nlistSizeBenchmark() -> " + biMap.getClass().getSimpleName());
 		long free = Runtime.getRuntime().freeMemory();
@@ -183,7 +254,7 @@ public class MemoryBenchmark {
 		System.out.println("Used mem: " + (free - freeAfter));		
 	}
 
-	private static void tableSizeBenchmark(Table<Integer, Integer, Integer> biMap) {
+	public static void tableSizeBenchmark(Table<Integer, Integer, Integer> biMap) {
 		new MemoryBenchmark();
 		
 		int columns = 100;
@@ -203,7 +274,7 @@ public class MemoryBenchmark {
 		System.out.println("Used mem: " + (free - freeAfter));		
 	}
 	
-	private static void loadingCacheSizeBenchmark(LoadingCache<Integer, Integer> loadingCache) throws ExecutionException {
+	public static void loadingCacheSizeBenchmark(LoadingCache<Integer, Integer> loadingCache) throws ExecutionException {
 		new MemoryBenchmark();
 		System.out.println("\nlistSizeBenchmark() -> " + loadingCache.getClass().getSimpleName());
 		long free = Runtime.getRuntime().freeMemory();
@@ -217,4 +288,18 @@ public class MemoryBenchmark {
 		System.out.println("Used mem: " + (free - freeAfter));			
 	}
 	
+
+	public static void concurrentSkipListMapSizeBenchmark(ConcurrentSkipListMap<Integer, Integer> concurrentSkipListMap) {
+		new MemoryBenchmark();
+		System.out.println("\nlistSizeBenchmark() -> " + concurrentSkipListMap.getClass().getSimpleName());
+		long free = Runtime.getRuntime().freeMemory();
+		long freeAfter;
+		
+		for(int i = 0; i < SIZE; i++) {
+			concurrentSkipListMap.put(i, i);
+		}
+		
+		freeAfter = Runtime.getRuntime().freeMemory();
+		System.out.println("Used mem: " + (free - freeAfter));		
+	}	
 }
